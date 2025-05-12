35fa61291ee6e48a7b26f21fbc65627877e8012c
Ivan Starčević <ivan.starcevic@leapwise.co>
2025-05-12 23:44:22 +0200

    add tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astrotutorialz.netlify.app/",
-  integrations: [preact()]
+  integrations: [preact()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
